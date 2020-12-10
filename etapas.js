256545fa4427bedf16c0b1369567d34bfc54f974
Carlos Santos <37418737+carlos0707@users.noreply.github.com>
2020-12-09 23:21:22 -0300

    Up
    Urna eletrônica em Javascript

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '55610',
+                nome: 'Bonfim Barreto',
+                partido: 'PSD',
+                fotos:[
+                    {url: 'bonfim-barreto-55610.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '40789',
+                nome: 'Isac moura',
+                partido: 'PT',
+                fotos:[
+                    {url: 'isaac-moura-40789.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '55',
+                nome: 'Batista',
+                partido: 'PSD',
+                vice: 'Bier',
+                fotos:[
+                    {url:'batista-da-mariana-55.jpg', legenda: 'Prefeito'},
+                    {url:'biew.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Dinho',
+                partido: 'PT',
+                vice: 'Chico',
+                fotos:[
+                    {url:'dinho-13.jpg', legenda: 'Prefeito'},
+                    {url:'chico.jpg', legenda:'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
